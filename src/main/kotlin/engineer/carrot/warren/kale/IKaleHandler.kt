a49b1c2b6e1db8138a319e58f7a4dd8a1b2f4a68
Sky Welch <git@bunnies.io>
2016-04-16 12:18:40 +0100

    Fix typo in IKaleHandler

--- src/main/kotlin/engineer/carrot/warren/kale/IKaleHandler.kt
+++ src/main/kotlin/engineer/carrot/warren/kale/IKaleHandler.kt
@@ -5,5 +5,5 @@ import engineer.carrot.warren.kale.irc.message.IMessage
 interface IKaleHandler<T: IMessage> {
     val messageType: Class<T>
 
-    fun handle(messageOne: T)
+    fun handle(message: T)
 }
