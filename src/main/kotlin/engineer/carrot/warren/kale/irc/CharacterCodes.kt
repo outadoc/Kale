f54ec6e348c49cfc9af9bcd21a99b815d38807ed
Sky Welch <git@bunnies.io>
2016-05-22 17:34:03 +0100

    Add case mapping utilities

--- src/main/kotlin/engineer/carrot/warren/kale/irc/CharacterCodes.kt
+++ src/main/kotlin/engineer/carrot/warren/kale/irc/CharacterCodes.kt
@@ -6,8 +6,8 @@ object CharacterCodes {
     val AT: Char = 0x40.toChar()
     val SPACE: Char = 0x20.toChar()
     val EXCLAM: Char = 0x21.toChar()
-    val LEFT_BRACKET: Char = 0x28.toChar()
-    val RIGHT_BRACKET: Char = 0x29.toChar()
+    val LEFT_ROUNDED_BRACKET: Char = 0x28.toChar()
+    val RIGHT_ROUNDED_BRACKET: Char = 0x29.toChar()
     val PLUS: Char = 0x2B.toChar()
     val COMMA: Char = 0x2C.toChar()
     val MINUS: Char = 0x2D.toChar()
@@ -15,6 +15,12 @@ object CharacterCodes {
     val SEMICOLON: Char = 0x3B.toChar()
     val EQUALS: Char = 0x3D.toChar()
     val BACKSLASH: Char = 0x5C.toChar()
+    val LEFT_STRAIGHT_BRACKET: Char = 0x5B.toChar()
+    val RIGHT_STRAIGHT_BRACKET: Char = 0x5D.toChar()
+    val CARET: Char = 0x5E.toChar()
+    val LEFT_CURLY_BRACKET: Char = 0x7B.toChar()
+    val RIGHT_CURLY_BRACKET: Char = 0x7D.toChar()
+    val TILDE: Char = 0x7E.toChar()
 
     val CTCP = '\u0001'
 }
---
+++ src/main/kotlin/engineer/carrot/warren/kale/irc/message/utility/CaseMapping.kt
@@ -0,0 +1,28 @@
+package engineer.carrot.warren.kale.irc.message.utility
+
+import engineer.carrot.warren.kale.irc.CharacterCodes
+
+enum class CaseMapping(val upperToLowerMapping: Map<Char, Char>) {
+    ASCII(mapOf()),
+    STRICT_RFC1459(mapOf(CharacterCodes.LEFT_STRAIGHT_BRACKET to CharacterCodes.LEFT_CURLY_BRACKET,
+                         CharacterCodes.RIGHT_STRAIGHT_BRACKET to CharacterCodes.RIGHT_CURLY_BRACKET)),
+    RFC1459(STRICT_RFC1459.upperToLowerMapping.plus(mapOf(CharacterCodes.CARET to CharacterCodes.TILDE)));
+
+    fun toLower(string: String): String {
+        val charArray = string.toCharArray()
+        for (i in 0..charArray.size-1) {
+            val replacement = upperToLowerMapping[charArray[i]]
+            if (replacement != null) {
+                charArray[i] = replacement
+            } else {
+                charArray[i] = charArray[i].toLowerCase()
+            }
+        }
+
+        return String(charArray)
+    }
+}
+
+fun equalsIgnoreCase(mapping: CaseMapping, lhs: String, rhs: String): Boolean {
+    return mapping.toLower(lhs) == mapping.toLower(rhs)
+}
