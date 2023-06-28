edf446c66185f947624147601298e84b3a7a6c59
Baptiste Candellier <baptiste@candellier.me>
2023-06-28 13:25:27 +0200

    Upgrade Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.6.21" apply false
-    kotlin("kapt") version "1.6.21" apply false
+    kotlin("multiplatform") version "1.8.22" apply false
+    kotlin("kapt") version "1.8.22" apply false
     id("org.jetbrains.kotlinx.kover") version "0.7.2" apply false
 }
